35040564cd6153dd54c95c6b9748c1f342c53840
Pieter van Ginkel <pvginkel@gmail.com>
2012-04-01 07:07:55 +0200

    * Added unit test for testing a problem expression with Flee.

---
+++ Expressions.Test/FleeLanguage/Compilation/CompilationFailures.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Expressions.Test.FleeLanguage.Compilation
+{
+    [TestFixture]
+    internal class CompilationFailures : TestBase
+    {
+        [Test]
+        [ExpectedException]
+        public void InfiniteLoop()
+        {
+            Resolve("true && false");
+        }
+    }
+}
