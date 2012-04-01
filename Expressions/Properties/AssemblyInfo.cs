d5fbc86c6e0b4bc29575222089e6a53fb87b0a62
Pieter van Ginkel <pvginkel@gmail.com>
2012-04-01 10:42:08 +0200

    * Cleaned up AssemblyInfo.cs.

--- Expressions/Properties/AssemblyInfo.cs
+++ Expressions/Properties/AssemblyInfo.cs
@@ -3,37 +3,20 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Expressions")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyCompany("Pieter van Ginkel")]
 [assembly: AssemblyProduct("Expressions")]
-[assembly: AssemblyCopyright("Copyright © HP 2012")]
+[assembly: AssemblyCopyright("Pieter van Ginkel © 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("e84f56bf-8a44-4787-acb5-0dc2f22b1f8f")]
 [assembly: InternalsVisibleTo("Expressions.Test, PublicKey=0024000004800000940000000602000000240000525341310004000001000100157527ab8ef03d584cd8fff60c6642b9172c679da194efc5591f3bf9ea6919ff5e905d624c1e2146e27f84478e4b6f985a24e556fcea3bd072b2761c077a5540900553040e8fd335b7fbf9080ba585055c9e9ae9a1ae1169c89120f99f149bb396d1f7796a73f8ae69734b807352a89b096fe540c602f48cb0a7a0de053bcea2")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
