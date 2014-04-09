6ba52b25665d4516dc270658d3b64bf42aa625e8
Pieter van Ginkel <pvginkel@gmail.com>
2014-04-09 10:21:21 +0200

    Created version 0.1.3.0.

--- Expressions/Properties/AssemblyInfo.cs
+++ Expressions/Properties/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("e84f56bf-8a44-4787-acb5-0dc2f22b1f8f")]
 [assembly: InternalsVisibleTo("Expressions.Test, PublicKey=0024000004800000940000000602000000240000525341310004000001000100157527ab8ef03d584cd8fff60c6642b9172c679da194efc5591f3bf9ea6919ff5e905d624c1e2146e27f84478e4b6f985a24e556fcea3bd072b2761c077a5540900553040e8fd335b7fbf9080ba585055c9e9ae9a1ae1169c89120f99f149bb396d1f7796a73f8ae69734b807352a89b096fe540c602f48cb0a7a0de053bcea2")]
 
-[assembly: AssemblyVersion("0.1.2.0")]
-[assembly: AssemblyFileVersion("0.1.2.0")]
+[assembly: AssemblyVersion("0.1.3.0")]
+[assembly: AssemblyFileVersion("0.1.3.0")]
